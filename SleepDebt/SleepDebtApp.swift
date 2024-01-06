3aa391eb82c20311282f944c9efc845e24a4a772
oliverhnat <oliver.hnat@gmail.com>
2024-01-06 11:07:48 +0100

    Initial Commit

---
+++ SleepDebt/SleepDebtApp.swift
@@ -0,0 +1,17 @@
+//
+//  SleepDebtApp.swift
+//  SleepDebt
+//
+//  Created by Oliver Hnát on 06.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct SleepDebtApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
